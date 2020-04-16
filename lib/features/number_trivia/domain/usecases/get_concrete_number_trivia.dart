d062d7e950f420cee7e7e1b8362fe17274845e0b
Prajwal S Belagavi <prajwalpspbelagavi@gmail.com>
2020-04-16 21:01:48 +0530

    implement abstract Usecase

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,22 +1,29 @@
+import 'package:architecture/core/error/failures.dart';
+import 'package:architecture/core/usecases/usecase.dart';
 import 'package:architecture/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../core/error/failures.dart';
 
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia implements Usecase<NumberTrivia,Params> {
   final NumberTriviaRepository repository;
 
   GetConcreteNumberTrivia(this.repository);
 
-  Future<Either<Failure, NumberTrivia>> call({
-    @required int number,
-  }) async {
-    return await repository.getConcreteNumberTrivia(number);
+  @override
+  Future<Either<Failure, NumberTrivia>> call(Params  params) async {
+    return await repository.getConcreteNumberTrivia(params.number);
   }
 
   Future<Either<Failure, NumberTrivia>> executeRandom() async {
     return await repository.getRandomNumberTrivia();
   }
+}
+
+class Params extends Equatable {
+  final int number;
+
+  Params({@required this.number}) : super([number]);
 }
