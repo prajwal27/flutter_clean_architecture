2d0fe3a1fd2d3c645ce27c0f054e4e19dadd69fb
Prajwal S Belagavi <prajwalpspbelagavi@gmail.com>
2020-04-16 19:16:48 +0530

    folders setup for architecture

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,22 @@
+import 'package:architecture/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/error/failures.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+
+  GetConcreteNumberTrivia(this.repository);
+
+  Future<Either<Failure, NumberTrivia>> execute({
+    @required int number,
+  }) async {
+    return await repository.getConcreteNumberTrivia(number);
+  }
+
+  Future<Either<Failure, NumberTrivia>> executeRandom() async {
+    return await repository.getRandomNumberTrivia();
+  }
+}
