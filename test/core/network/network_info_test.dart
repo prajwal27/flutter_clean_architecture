a362cc14ea60d3ac78a6f460b32c4f7f6e06f221
Prajwal S Belagavi <prajwalpspbelagavi@gmail.com>
2020-04-17 19:14:38 +0530

    Network Info: tests

---
+++ test/core/network/network_info_test.dart
@@ -0,0 +1,32 @@
+import 'package:architecture/core/network/network_info.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:data_connection_checker/data_connection_checker.dart';
+
+class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
+
+void main() {
+  NetworkInfoImpl networkInfoImpl;
+  MockDataConnectionChecker mockDataConnectionChecker;
+
+  setUp(() {
+    mockDataConnectionChecker = MockDataConnectionChecker();
+    networkInfoImpl = NetworkInfoImpl(mockDataConnectionChecker);
+  });
+
+  group('isConnected', () {
+    test(
+      'should forward the call to DataConnection.has Connection',
+      () async {
+        // arrange
+        final tHasConnectionFuture = Future.value(true);
+        when(mockDataConnectionChecker.hasConnection).thenAnswer((_) => tHasConnectionFuture);
+        // act
+        final result = networkInfoImpl.isConnected;
+        // assert
+        verify(mockDataConnectionChecker.hasConnection);
+        expect(result, tHasConnectionFuture);
+      },
+    );
+  });
+}
